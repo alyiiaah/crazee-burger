3ba49dabfca77fdff389551b5356e349f744344f
Naoëlle <alyiiaah@live.fr>
2023-06-08 13:19:19 +0200

    add my firstname

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Salut Naoëlle
+          
         </p>
         <a
           className="App-link"

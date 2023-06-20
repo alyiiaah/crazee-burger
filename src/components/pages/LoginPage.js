77c2e7a252c4b728a1818b7cf41f72840a3f3f0b
Naoëlle <alyiiaah@live.fr>
2023-06-21 00:24:12 +0200

    ✨ F01 LOGIN> add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function LoginPage() {
+  //State
+
+  const [inputValue, setInputValue] = useState("Bob");
+
+  //Comportements
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  //affichage (render)
   return (
-    <div>LoginPage</div>
-  )
+    <div>
+      <h1>Bienvenus chez nous!</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button> Accédez à votre espace</button>
+      </form>
+    </div>
+  );
 }

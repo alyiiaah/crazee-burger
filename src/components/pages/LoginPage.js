6695ee45b485011debda1ae9fea7d0c5cc548d30
Naoëlle <alyiiaah@live.fr>
2023-07-13 20:20:21 +0200

    ✨ F01: LOGIN > add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -4,36 +4,39 @@ import { useState } from "react";
 export default function LoginPage() {
   //State
 
-  const [inputValue, setInputValue] = useState("Bob");
+const [inputValue, setInputValue] = useState("")
+
+  
 
   //Comportements
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`Bonjour ${inputValue}`);
-    setInputValue("");
-  };
+    event.preventDefault()
+    alert (`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
 
   const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
+    setInputValue(event.target.value)
+
+  }
+
+  
 
   //affichage (render)
   return (
     <div>
-      <h1>Bienvenus chez nous!</h1>
+      <h1>Bienvenue chez nous </h1>
       <br />
       <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          value={inputValue}
+        <form action="submit" onSubmit={handleSubmit}>
+          <input value={inputValue} 
           onChange={handleChange}
-          type="text"
-          placeholder="Entrez votre prénom..."
-          required
-        />
-        <button> Accédez à votre espace</button>
-      </form>
-    </div>
-  );
+          type="text" 
+          placeholder="Entrez votre prénom.." 
+          required/>
+          <button> Accédez à votre éspace</button>
+        </form>
+      </div>
+  )
 }
